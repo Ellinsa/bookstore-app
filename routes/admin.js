6da7450e65f9182a16ba6054433c2ae4101cea32
BlueSharkBites <toriatovawebdev+1@gmail.com>
2023-01-10 15:35:27 +0400

    add more details

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,13 @@
 const express = require('express');
-
+const path = require('path');
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="post"><input type="text" name="title"/><button type="submit">Submit</button></form>'
-  );
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const http = require('http');
 const fs = require('fs');
-
+const path = require('path');
+const rootDir = require('./util/path');
 const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(rootDir, 'public')));
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(shopRoutes);
-app.use(adminRoutes);
+
+//filtered path
+app.use('/admin', adminRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  res.sendFile(path.join(rootDir, 'views', 'not-found.html'));
 });
 
 app.listen(3000);
